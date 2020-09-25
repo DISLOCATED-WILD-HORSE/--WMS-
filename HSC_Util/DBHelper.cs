ac2a0348c27bd76c1aecb6d75a89d31cb03c1800
HS08741 <HS08741@HSIT022104>
2020-09-25 14:13:33 +0800

    Revert "还原优化代码结构"
    This reverts commit 0a643b071dd06c8f379b442b9cc3d0ac757bd6d7.

--- HSC_Util/DBHelper.cs
+++ HSC_Util/DBHelper.cs
@@ -1,4 +1,5 @@
-﻿using SqlSugar;
+﻿using HSC_Util;
+using SqlSugar;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace HSC_SYPrintSystem
     public class DBHelper<T> where T : class,new()
     {
         public static string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
-
+        
         public DBHelper()
         {
             Db = new SqlSugarClient(config);
@@ -19,6 +20,7 @@ namespace HSC_SYPrintSystem
                 //if (Db.TempItems.ContainsKey(StartTime)) Db.TempItems[StartTime] = DateTime.Now;
                 //if (!Db.TempItems.ContainsKey(StartTime)) Db.TempItems.Add(StartTime, DateTime.Now);
                 //System.Windows.Forms.MessageBox.Show(sql);
+                //LogerHelper.WriteLog("SqlSugar语句:" + sql);
                 //LogHelper.Debug("请求标识：" + PDASession.Current.Request_Id + "\r\n SqlSugar语句：" + sql + "\r\n" + Db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
             };
         }
